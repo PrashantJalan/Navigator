c2479e11d1062ffad5b5b89d0e95a0b8641ac8ef
Prashant Jalan <pj.prashantjalan@gmail.com>
2014-04-07 03:08:18 +0530

    Changed Node class

--- Node.java
+++ Node.java
@@ -1,4 +1,8 @@
 package com.enable.navigator;
+
+import java.util.Set;
+import java.util.TreeSet;
+
 /*
  * Node class for containing the Graph
  */
@@ -6,6 +10,9 @@ package com.enable.navigator;
 public class Node {
 	public int x = 0;
 	public int y = 0;
+	public int index = 0;
+	public String label = "empty";
+	public double scale = 0.25;
 	public Node left = null;
 	public Node right = null;
 	public Node up = null;
@@ -14,9 +21,61 @@ public class Node {
 	public Node upLeft = null;
 	public Node downLeft = null;
 	public Node downRight = null;
+	public Boolean active = false;
+	public Boolean exists = true;
 	
 	public double getDistance(Node node){
 		double dist = Math.sqrt((node.y - this.y)*(node.y - this.y) + (node.x - this.x)*(node.x - this.x));
 		return dist;
+	}
+	
+	//Upto a List of Nodes doing BFS till 'level' neighbours. level>=1
+	public Set<Integer> getNodes(int level)	{
+		Set<Integer> li = new TreeSet<Integer>();
+		Set<Integer> temp;
+		li.add(this.index);
+		if (level>=1)	{
+			if (this.left != null)	{
+				temp = this.left.getNodes(level-1);
+				li.addAll(temp);
+			}
+			if (this.right != null)	{
+				temp = right.getNodes(level-1);
+				li.addAll(temp);
+			}
+			if (this.up != null)	{
+				temp = up.getNodes(level-1);
+				li.addAll(temp);
+			}
+			if (this.down != null)	{
+				temp = down.getNodes(level-1);
+				li.addAll(temp);
+			}
+			if (this.upRight != null)	{
+				temp = upRight.getNodes(level-1);
+				li.addAll(temp);
+			}
+			if (this.downRight != null)	{
+				temp = downRight.getNodes(level-1);
+				li.addAll(temp);
+			}
+			if (this.upLeft != null)	{
+				temp = upLeft.getNodes(level-1);
+				li.addAll(temp);
+			}
+			if (this.downLeft != null)	{
+				temp = downLeft.getNodes(level-1);
+				li.addAll(temp);
+			}
+			if (this.right != null)	{
+				temp = right.getNodes(level-1);
+				li.addAll(temp);
+			}
+			if (this.right != null)	{
+				temp = right.getNodes(level-1);
+				li.addAll(temp);
+			}
+		}
+		return li;
 	}
 }
